71ab1ff014f98501e877616e87213c021ec5a643
name1e5s <name1e5s@qq.com>
2021-01-11 15:41:46 +0800

    [regfile] add register file

---
+++ src/main/scala/cecike/core/backend/register/RegisterFile.scala
@@ -0,0 +1,53 @@
+package cecike.core.backend.register
+
+import chisel3._
+import chisel3.util._
+import cecike.core.common.Constants._
+import cecike.utils._
+
+class RegisterFileReadPort extends Bundle {
+  val addr = Input(UInt(physicalRegisterAddressWidth.W))
+  val data = Output(UInt(xLen.W))
+}
+
+class RegisterFileWritePort extends Bundle {
+  val valid = Input(Bool())
+  val addr = Input(UInt(physicalRegisterAddressWidth.W))
+  val data = Input(UInt(xLen.W))
+}
+
+class RegisterFileIO extends Bundle {
+  val readPort = Vec(2 * issueWidth, new RegisterFileReadPort)
+  val writePort = Vec(issueWidth, new RegisterFileWritePort)
+}
+
+class RegisterFile(bypass: Seq[Boolean]) extends Module {
+  require(bypass.length == issueWidth)
+  require(bypass.contains(true))
+  val io = IO(new RegisterFileIO)
+
+  val bypassedWritePort = (bypass zip io.writePort).filter(_._1).map(_._2)
+
+  val registerFile = Mem(physicalRegisterNum, UInt(xLen.W))
+
+  // Write
+  io.writePort.foreach { p =>
+    when(p.valid) {
+      registerFile.write(p.addr, p.data)
+    }
+  }
+
+  // Read
+  io.readPort.foreach { p =>
+    val addr = p.addr
+    val addrNotZero = p.addr.orR()
+    val data = Mux(addrNotZero, registerFile.read(addr), 0.U)
+    val bypassedResults = bypassedWritePort.map { p =>
+      val valid = p.valid && p.addr === addr && addrNotZero
+      val data = p.data
+      (valid, data)
+    }
+    val result = MuxCase(data, bypassedResults)
+    p.data := result
+  }
+}
