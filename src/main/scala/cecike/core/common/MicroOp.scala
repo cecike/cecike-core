390d19ca32e1ff41f5fbc5421a9370455c60f594
name1e5s <name1e5s@qq.com>
2021-01-21 19:51:36 +0800

    [backend] move *MicroOp into the same file

--- src/main/scala/cecike/core/common/MicroOp.scala
+++ src/main/scala/cecike/core/common/MicroOp.scala
@@ -2,6 +2,7 @@ package cecike.core.common
 
 import chisel3._
 import cecike.core.common.Constants._
+import cecike.utils._
 
 class MicroOp extends Bundle {
   val valid = Bool()
@@ -49,6 +50,60 @@ object MicroOp {
 
     microOp.instType := InstructionType.X
     microOp.fuType := FunctionUnitType.FU_ALU
+
+    microOp
+  }
+}
+
+class SourceRegisterInfo extends Bundle {
+  val busy = Bool()
+  val addr = UInt(physicalRegisterAddressWidth.W)
+}
+
+class IssueMicroOp extends Bundle {
+  // Micro Op Info
+  val pc = UInt(xLen.W)
+
+  val branchTag = UInt(maxBranchCount.W)
+  val branchPredictionInfo = new BranchPredictionInfo
+
+  val instType = UInt(InstructionType.instTypeWidth.W)
+  val fuType = UInt(FunctionUnitType.fuTypeWidth.W)
+  val fuOp = UInt(functionUnitOpWidth.W)
+
+  val rs1Info = new SourceRegisterInfo
+  val rs2Info = new SourceRegisterInfo
+  val immediate = UInt(xLen.W)
+
+  val rdInfo = UndirectionalValid(UInt(physicalRegisterAddressWidth.W))
+
+  val robIndex = UInt(robAddressWidth.W)
+}
+
+object IssueMicroOp {
+  def apply(microOpIn: MicroOp) = {
+    val microOp = Wire(new IssueMicroOp)
+    microOp.pc := microOpIn.pc
+
+    microOp.branchTag := microOpIn.branchTag
+    microOp.branchPredictionInfo := microOpIn.branchPredictionInfo
+
+    microOp.instType := microOpIn.instType
+    microOp.fuType := microOpIn.fuType
+    microOp.fuOp := microOpIn.fuOp
+
+    microOp.rs1Info.addr := microOpIn.physicalRs1
+    microOp.rs1Info.busy := microOpIn.physicalRs1Busy
+
+    microOp.rs2Info.addr := microOpIn.physicalRs2
+    microOp.rs2Info.busy := microOpIn.physicalRs2Busy
+
+    microOp.immediate := microOpIn.immediate
+
+    microOp.rdInfo.bits := microOpIn.physicalRd
+    microOp.rdInfo.valid := microOpIn.rdValid
+
+    microOp.robIndex := microOpIn.robIndex
 
     microOp
   }
